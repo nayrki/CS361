5906577f87babb51b8e1c3bb7003f4b7e857d380
luansongjian <luans@onid.oregonstate.edu>
2015-08-05 21:41:45 -0700

    multiple commit

--- Sample/app/src/main/java/com/cs361/luan/sample/MainActivity.java
+++ Sample/app/src/main/java/com/cs361/luan/sample/MainActivity.java
@@ -1,16 +1,64 @@
 package com.cs361.luan.sample;
 
+import android.os.*;
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        triggerActivityView();
+    }
+
+    private void triggerActivityView() {
+        setContentView(R.layout.activity_main);
+
+        Button mainRegisterButton = (Button) findViewById(R.id.main_register_button);
+        mainRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                triggerRegisterView();
+            }
+        });
+
+        Button mainLoginButton = (Button) findViewById(R.id.main_login_button);
+        mainLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                triggerLoginView();
+            }
+        });
+    }
+
+    private void triggerRegisterView() {
+        setContentView(R.layout.register_view);
+
+        Button registerHomeButton = (Button) findViewById(R.id.register_home_button);
+        registerHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                triggerActivityView();
+            }
+        });
+    }
+
+    private void triggerLoginView() {
         setContentView(R.layout.login_view);
+
+        Button loginQuitButton = (Button) findViewById(R.id.login_quit_button);
+        loginQuitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Process.killProcess(Process.myPid());
+                System.exit(0);
+            }
+        });
     }
 
     @Override
@@ -34,4 +82,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
